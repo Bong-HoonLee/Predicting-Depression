d66518ee7defcc89570151cf83710722c83b1d41
chirichidi <tolessnoise@gmail.com>
2023-12-02 19:04:26 +0900

    refactoring: moved step functions from model to trainer

--- models/ann.py
+++ models/ann.py
@@ -1,12 +1,10 @@
 import torch
-import torchmetrics
+
 
 import torch.nn as nn
 
-from torch.utils.data import DataLoader
-from models.modelable import Modelable
 
-class ANN(Modelable, nn.Module):
+class ANN(nn.Module):
     def __init__(
             self, 
             perceptron = (4, 128, 1),
@@ -23,33 +21,9 @@ class ANN(Modelable, nn.Module):
         
         self.history = []
 
-
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.lin1(x)
         x = self.activation(x)
         x = self.dropout(x)
         x = self.lin2(x)
         return x
-    
-
-    def training_step(self, dataloader: DataLoader, loss_function, optimizer: torch.optim.Optimizer, device, metric: torchmetrics.metric.Metric) -> float:
-        self.train()
-        for X, y in dataloader:
-            X, y = X.to(device), y.to(device)
-            y_hat = self.forward(X)
-            loss = loss_function(y_hat, y)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            metric.update(y_hat, y)
-
-    def validation_step(self, dataloader: DataLoader, device, metric: torchmetrics.metric.Metric):
-        self.eval()
-        with torch.inference_mode():
-            for X, y in dataloader:
-                X, y = X.to(device), y.to(device)
-                y_hat = self.forward(X)
-                metric.update(y_hat, y)
-
-    def test_step(self, dataloader: DataLoader):
-        pass
