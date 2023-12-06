5301af176ea6516fa43a4eb4b3f12d9852f2fcd9
chirichidi <tolessnoise@gmail.com>
2023-12-06 17:07:04 +0900

    bugfix: fixed accummulated weight, bias on trainning

--- models/ann.py
+++ models/ann.py
@@ -1,9 +1,11 @@
 import torch.nn as nn
+from copy import deepcopy
+
 
 class ANN(nn.Module):
     def __init__(self, module_list: nn.ModuleList = []):
         super(ANN, self).__init__()
-        self.module_list = module_list
+        self.module_list = deepcopy(module_list)
 
     def forward(self, x):
         for module in self.module_list:
